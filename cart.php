8214c25c19a8c5be4980f967a99dc67da3856814
Prajwol Mishra <mishraprajwol23@gmail.com>
2024-11-17 00:40:09 +0545

    commit 3

--- cart.php
+++ cart.php
@@ -20,65 +20,86 @@ include('authenticate.php');
 
 <div class="py-5">
     <div class="container">
-        <div class="card card-body shadow">
+        <div class="">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="row align-items-center">
-                        <div class="col-md-5">
-                            <h6>Product</h6>
-                        </div>
-                        <div class="col-md-3">
-                            <h6>Price</h6>
-                        </div>
-                        <div class="col-md-2">
-                            <h6>Quantity</h6>
-                        </div>
-                        <div class="col-md-2">
-                            <h6>Action</h6>
-                        </div>
-                    </div>
-
-                     <div id="mycart">
-
+                    <div id="mycart">
                     <?php
 
                     $items = getCartItems();
 
-                     foreach ($items as $citem) {
+                    if (mysqli_num_rows($items) > 0) 
+                    {
                         ?>
-                        <div class="card product-data shadow-sm mb-3">
                         <div class="row align-items-center">
-                            <div class="col-md-2">
-                                <img src="uploads/<?= $citem['image']; ?>" alt="Product Image" width="80px">
+                            <div class="col-md-5">
+                                <h6>Product</h6>
                             </div>
                             <div class="col-md-3">
-                                <h5><?= $citem['name']; ?></h5>
-                            </div>
-                            <div class="col-md-3">
-                                <h5>Rs. <?= $citem['selling_price']; ?></h5>
+                                <h6>Price</h6>
                             </div>
                             <div class="col-md-2">
-                                <input type="hidden" class="prodId" value="<?= $citem['prod_id'] ?>">
-                                <div class="input-group mb-3" style="width: 130px;">
-                                    <button class="input-group-text decrement-btn updateQty">-</button>
-                                    <input type="text" class="form-control text-center input-qty bg-white"
-                                        value="<?= $citem['prod_qty']; ?>" disabled>
-                                    <button class="input-group-text increment-btn updateQty">+</button>
-                                </div>
+                                <h6>Quantity</h6>
                             </div>
                             <div class="col-md-2">
-                                <button class="btn btn-danger btn-sm deleteItem" value="<?= $citem['cid'] ?>">
-                                <i class="fa fa-trash me-2"></i>Remove</button>
+                                <h6>Remove</h6>
                             </div>
-                        </div>
                         </div>
 
+                        <div id="">
+                            <?php
+
+                            foreach ($items as $citem) {
+                                ?>
+                                <div class="card product-data shadow-sm mb-3">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-2">
+                                            <img src="uploads/<?= $citem['image']; ?>" alt="Product Image" width="80px">
+                                        </div>
+                                        <div class="col-md-3">
+                                            <h5><?= $citem['name']; ?></h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <h5>Rs. <?= $citem['selling_price']; ?></h5>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <input type="hidden" class="prodId" value="<?= $citem['prod_id'] ?>">
+                                            <div class="input-group mb-3" style="width: 130px;">
+                                                <button class="input-group-text decrement-btn updateQty">-</button>
+                                                <input type="text" class="form-control text-center input-qty bg-white"
+                                                    value="<?= $citem['prod_qty']; ?>" disabled>
+                                                <button class="input-group-text increment-btn updateQty">+</button>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <button class="btn btn-danger btn-sm deleteItem" value="<?= $citem['cid'] ?>">
+                                                <i class="fa fa-trash me-2"></i>Remove</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <?php
+                            }
+
+                            ?>
+                        </div>
+                        <div class="float-end">
+                            <a href="checkout.php" class="btn btn-outline-primary">Proceed to checkout</a>
+                        </div>
                         <?php
-                     }
+                    } 
+
+                    else 
+                    {
+                        ?>
+                        <div class="card card-body shadow text-center">
+                            <h4 class="py-3">Your cart is empty.</h4>
+                        </div>
+                        <?php
+                    }
 
                     ?>
-                     </div>
-
+                    </div>
                 </div>
             </div>
         </div>
---
+++ functions/placeorder.php
@@ -0,0 +1,88 @@
+<?php
+
+session_start();
+
+include('../config/dbcon.php');
+
+
+if (isset($_SESSION['auth'])) 
+{
+    if (isset($_POST['placeOrderBtn'])) 
+    {
+        $name = mysqli_real_escape_string($con,$_POST['name']);
+        $email = mysqli_real_escape_string($con,$_POST['email']);
+        $phone = mysqli_real_escape_string($con,$_POST['phone']);
+        $pincode = mysqli_real_escape_string($con,$_POST['pincode']);
+        $address = mysqli_real_escape_string($con,$_POST['address']);
+        $payment_mode = mysqli_real_escape_string($con,$_POST['payment_mode']);
+        $payment_id = mysqli_real_escape_string($con,$_POST['payment_id']);
+        
+        if($name == "" || $email=="" || $phone== "" || $pincode == "" || $address == "")
+        {
+            $_SESSION['message'] = "All fields are mandatory.";
+            header('Location: ../checkout.php');
+            exit (0);
+        }
+
+        $userId = $_SESSION['auth_user']['user_id'];
+        $query = "SELECT c.id as cid, c.prod_id, c.prod_qty, p.id as pid, p.name, p.image, p.selling_price 
+                  FROM carts c, products p WHERE c.prod_id = p.id AND c.user_id='$userId' ORDER BY c.id DESC";
+
+        $query_run = mysqli_query($con,$query);
+   
+        $totalPrice = 0;
+
+        foreach ($query_run as $citem) 
+        {
+            $totalPrice += $citem['selling_price'] * $citem['prod_qty'];
+        }
+
+        $tracking_no = "unicart".rand(1111,9999).substr($phone,2);
+
+        //to store the order details form in orders table
+        $insert_query = "INSERT INTO orders (tracking_no, user_id, name, email, phone, address, pincode, total_price, 
+                  payment_mode, payment_id) VALUES ('$tracking_no', '$userId', '$name', '$email', '$phone', 
+                  '$address', '$pincode', '$totalPrice', '$payment_mode', '$payment_id' )";
+
+        $insert_query_run = mysqli_query($con,$insert_query);
+
+        if($insert_query_run)
+        {
+            $order_id = mysqli_insert_id($con);
+
+            foreach ($query_run as $citem) 
+            {
+            
+            $prod_id = $citem['prod_id'];
+            $prod_qty = $citem['prod_qty'];
+            $price = $citem['selling_price'];
+
+            //to insert ordered item
+            $insert_items_query = "INSERT INTO order_items (order_id, prod_id, qty, price) 
+                                   VALUES ('$order_id', '$prod_id', '$prod_qty', '$price') ";
+
+            $insert_items_query_run = mysqli_query($con,$insert_items_query); 
+
+            }
+
+            //emptying cart after user places order
+            $deleteCartQuery = "DELETE FROM carts WHERE user_id = '$userId' ";
+            $deleteCartQuery_run = mysqli_query($con,$deleteCartQuery);
+ 
+            $_SESSION['message'] = "Order placed successfully";
+            header('Location: ../my-orders.php');
+            die();
+
+        }
+
+        else{
+
+        }
+
+    }
+}
+
+else{
+    header('Location: ../index.php');
+}
+?>
